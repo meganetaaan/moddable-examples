431e2ca8edf9e3d416eb6a4932904052dd48e309
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2019-05-08 09:08:04 +0900

    Fix core panic on sound

--- bongo/main.js
+++ bongo/main.js
@@ -12,9 +12,9 @@
  *
  */
 
-/* global global, Application, Skin, Label */
+/* global global, trace */
 
-import {} from 'piu/MC'
+import { Content, Texture, Skin, Label, Application, Style } from 'piu/MC'
 import Resource from 'Resource'
 import AudioOut from 'pins/i2s'
 
@@ -25,29 +25,13 @@ const sounds = {
 
 const speaker = global.speaker
 speaker.start()
-let playing = {
-  0: false,
-  1: false
-}
-
-speaker.callback = function (stream) {
-  trace(`stop: ${stream}\n`)
-  playing[stream] = false
-}
 
 function playSound (key, stream) {
   const sound = sounds[key]
   if (sound != null) {
     const speaker = global.speaker
-    if (playing[stream]) {
-      trace(`interrupt: ${stream}\n`)
-      return
-    }
     trace(`queue_s: ${stream}\n`)
-    playing[stream] = true
-    speaker.enqueue(stream, AudioOut.Flush)
     speaker.enqueue(stream, AudioOut.Samples, sound)
-    speaker.enqueue(stream, AudioOut.Callback, stream)
     trace(`queue_e: ${stream}\n`)
   }
 }
@@ -148,7 +132,7 @@ buttonC.onChanged = function () {
   application.content('leftHand').variant = up ? 0 : 1
   // play sound
   if (up === 0) {
-    playSound('high', 0)
+    playSound('high', 1)
   }
 }
 
