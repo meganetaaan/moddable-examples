6120b0f6d0ce6909c48e130655ff4f55d2121991
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2019-05-17 09:17:04 +0900

    Add plant logger

---
+++ plant-logger/main.js
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2019 Shinya Ishikawa
+ */
+
+import Timer from 'timer'
+
+import Analog from 'pins/analog'
+import Digital from 'pins/digital'
+
+const ANALOG_READ_PIN = 26
+const DIGITAL_READ_PIN = 36
+/* global trace */
+
+const INTERVAL = 1000
+
+Timer.repeat((_) => {
+  let value1 = Analog.read(ANALOG_READ_PIN)
+  let value2 = Digital.read(DIGITAL_READ_PIN)
+  trace(`value1: ${value1}, value2: ${value2}`)
+}, INTERVAL)
