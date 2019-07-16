0e13f76c364305f29f3f7fd85b5910c62bc40d3c
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2019-07-17 01:25:07 +0900

    Use piu

--- unit/env/dht12.js
+++ unit/env/dht12.js
@@ -2,64 +2,6 @@
  * Copyright (c) 2019 Shinya Ishikawa
  *
  */
-
-/*
-#include "DHT12.h"
-
-DHT12::DHT12(uint8_t scale,uint8_t id)
-{
-	if (id==0 || id>126) _id=0x5c;
-	else _id=id;
-	if (scale==0 || scale>3) _scale=CELSIUS;
-	else _scale=scale;
-}
-
-uint8_t DHT12::read()
-{
-	Wire.beginTransmission(_id);
-	Wire.write(0);
-	if (Wire.endTransmission()!=0) return 1;
-	Wire.requestFrom(_id, (uint8_t)5);
-	for (int i=0;i<5;i++) {
-		datos[i]=Wire.read();
-	};
-	delay(50);
-	if (Wire.available()!=0) return 2;
-	if (datos[4]!=(datos[0]+datos[1]+datos[2]+datos[3])) return 3;
-	return 0;
-}
-
-float DHT12::readTemperature(uint8_t scale)
-{
-	float resultado=0;
-	uint8_t error=read();
-	if (error!=0) return (float)error/100;
-	if (scale==0) scale=_scale;
-	switch(scale) {
-		case CELSIUS:
-			resultado=(datos[2]+(float)datos[3]/10);
-			break;
-		case FAHRENHEIT:
-			resultado=((datos[2]+(float)datos[3]/10)*1.8+32);
-			break;
-		case KELVIN:
-			resultado=(datos[2]+(float)datos[3]/10)+273.15;
-			break;
-	};
-	return resultado;
-}
-
-float DHT12::readHumidity()
-{
-	float resultado;
-	uint8_t error=read();
-	if (error!=0) return (float)error/100;
-	resultado=(datos[0]+(float)datos[1]/10);
-	return resultado;
-}
-
-*/
-
 import I2C from 'pins/i2c'
 
 const SCALE = {
@@ -71,7 +13,6 @@ const SCALE = {
 export default class DHT12 extends I2C {
   constructor (dictionary = { address: 0x5c, scale: SCALE.CELSIUS }) {
     super(dictionary)
-    // this._datos = new Uint8Array(5)
     this._scale = dictionary.scale
   }
 
@@ -85,6 +26,8 @@ export default class DHT12 extends I2C {
         return (bytes[2] + bytes[3] / 10) * 1.8 + 32
       case SCALE.KELVIN:
         return (bytes[2] + bytes[3] / 10) + 273.15
+      default:
+        throw new Error('Scale not supported')
     }
   }
 
--- unit/env/main.js
+++ unit/env/main.js
@@ -1,10 +1,77 @@
-/* global trace */
+/* global trace, hls, max, min */
 
+import { Application, Style, Skin, Label, Behavior, Column } from 'piu/MC'
 import DHT12 from './dht12'
-import Timer from 'timer'
 
 const dht12 = new DHT12()
+const FONT = 'OpenSans-Semibold-16'
+const INTERVAL = 2000
 
-Timer.repeat(() => {
-  trace(`Temperature: ${dht12.readTemperature()}\nHumidity: ${dht12.readHumidity()}\n`)
-}, 2000)
+function temperatureToColor (temperature) {
+  const clampedTemp = max(min(temperature, 0), 40)
+  const hue = (clampedTemp * 270) / 40
+  return hls(hue, 1, 0.4)
+}
+
+const HumidityLabel = Label.template((_) => ({
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+  loop: true,
+  interval: INTERVAL,
+  skin: new Skin({ fill: 'gray' }),
+  style: new Style({ font: FONT }),
+  Behavior: class HumidityBehavior extends Behavior {
+    onDisplaying (content) {
+      trace('hum label start')
+      content.start()
+    }
+    onTimeChanged (content) {
+      // const humidity = dht12.readHumidity()
+      content.string = `${50}%`
+    }
+  }
+}))
+
+const TemperatureLabel = Label.template((_) => ({
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+  interval: INTERVAL,
+  loop: true,
+  style: new Style({ font: FONT, color: 'white' }),
+  createSkin (temperature) {
+    const fill = temperatureToColor(temperature)
+    return new Skin({ fill })
+  },
+  update (temperature) {
+    this.string = `${temperature}C`
+    this.skin = this.createSkin(temperature)
+  },
+  Behavior: class TemperatureBehavior extends Behavior {
+    onDisplaying (content) {
+      trace('temp label start')
+      content.start()
+    }
+    onTimeChanged (content) {
+      const temperature = dht12.readTemperature()
+      content.update(temperature)
+    }
+  }
+}))
+
+const application = new Application(null, {
+  contents: [
+    new Column(null, {
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      contents: [new HumidityLabel(), new TemperatureLabel()]
+    })
+  ]
+})
+
+trace(application)
