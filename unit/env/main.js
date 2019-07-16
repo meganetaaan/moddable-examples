f65bf43e3904bbc9e4dffd1daaa56080cbb15f25
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2019-07-17 02:04:19 +0900

    Fix piu codes

--- unit/env/main.js
+++ unit/env/main.js
@@ -1,16 +1,26 @@
-/* global trace, hls, max, min */
+/* global trace */
 
 import { Application, Style, Skin, Label, Behavior, Column } from 'piu/MC'
+import { hsl, rgb } from 'piu/All'
 import DHT12 from './dht12'
 
 const dht12 = new DHT12()
 const FONT = 'OpenSans-Semibold-16'
 const INTERVAL = 2000
+const TEMP_MAX = 35
+const TEMP_MIN = 5
+const TEMP_RANGE = TEMP_MAX - TEMP_MIN
+const HUE_RANGE = 270
 
 function temperatureToColor (temperature) {
-  const clampedTemp = max(min(temperature, 0), 40)
-  const hue = (clampedTemp * 270) / 40
-  return hls(hue, 1, 0.4)
+  const clampedTemp = Math.max(Math.min(temperature, TEMP_MAX), TEMP_MIN)
+  const hue = ((TEMP_MAX - clampedTemp) * HUE_RANGE) / TEMP_RANGE
+  return hsl(hue, 1, 0.5)
+}
+
+function createSkin (temperature) {
+  const fill = temperatureToColor(temperature)
+  return new Skin({ fill })
 }
 
 const HumidityLabel = Label.template((_) => ({
@@ -19,17 +29,17 @@ const HumidityLabel = Label.template((_) => ({
   left: 0,
   right: 0,
   loop: true,
-  interval: INTERVAL,
-  skin: new Skin({ fill: 'gray' }),
-  style: new Style({ font: FONT }),
+  interval: 1000,
+  skin: new Skin({ fill: rgb(22, 22, 22) }),
+  style: new Style({ font: FONT, color: 'white' }),
   Behavior: class HumidityBehavior extends Behavior {
     onDisplaying (content) {
       trace('hum label start')
       content.start()
     }
     onTimeChanged (content) {
-      // const humidity = dht12.readHumidity()
-      content.string = `${50}%`
+      const humidity = dht12.readHumidity()
+      content.string = `${humidity.toFixed(1)}%`
     }
   }
 }))
@@ -42,22 +52,18 @@ const TemperatureLabel = Label.template((_) => ({
   interval: INTERVAL,
   loop: true,
   style: new Style({ font: FONT, color: 'white' }),
-  createSkin (temperature) {
-    const fill = temperatureToColor(temperature)
-    return new Skin({ fill })
-  },
-  update (temperature) {
-    this.string = `${temperature}C`
-    this.skin = this.createSkin(temperature)
-  },
   Behavior: class TemperatureBehavior extends Behavior {
+    update (content, temperature) {
+      content.string = `${temperature.toFixed(1)}C`
+      content.skin = createSkin(temperature)
+    }
     onDisplaying (content) {
       trace('temp label start')
       content.start()
     }
     onTimeChanged (content) {
       const temperature = dht12.readTemperature()
-      content.update(temperature)
+      content.delegate('update', temperature)
     }
   }
 }))
@@ -69,7 +75,7 @@ const application = new Application(null, {
       bottom: 0,
       left: 0,
       right: 0,
-      contents: [new HumidityLabel(), new TemperatureLabel()]
+      contents: [new TemperatureLabel(), new HumidityLabel()]
     })
   ]
 })
