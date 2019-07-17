b920910995f27c6125517fdde05b35d131167827
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2019-07-17 09:50:53 +0900

    Add platform settings

--- unit/env/main.js
+++ unit/env/main.js
@@ -15,7 +15,7 @@ const HUE_RANGE = 270
 function temperatureToColor (temperature) {
   const clampedTemp = Math.max(Math.min(temperature, TEMP_MAX), TEMP_MIN)
   const hue = ((TEMP_MAX - clampedTemp) * HUE_RANGE) / TEMP_RANGE
-  return hsl(hue, 1, 0.5)
+  return hsl(hue, 1, 0.3)
 }
 
 function createSkin (temperature) {
