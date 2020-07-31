e48e3f97aeb1a0dbd8a14f58624d31ffa102483a
root <root@sskw-x1.localdomain>
2020-07-31 15:39:13 +0000

    Add SHT3X driver

---
+++ unit/env/sht3x.js
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2019 Shinya Ishikawa
+ *
+ */
+import I2C from 'pins/i2c'
+import Timer from 'timer'
+
+const REGISTORY = {
+  READ: [0x2C, 0x06]
+};
+Object.freeze(REGISTORY);
+
+const SCALE = {
+  CELSIUS: 1,
+  KELVIN: 2,
+  FAHRENHEIT: 3
+}
+Object.freeze(SCALE);
+
+export default class SHT3X extends I2C {
+  constructor (dictionary = { address: 0x44, scale: SCALE.CELSIUS }) {
+    super(dictionary)
+    this._scale = dictionary.scale
+  }
+
+  readEnvironment (s = this._scale) {
+    this.write(...REGISTORY.READ)
+    Timer.delay(20);
+    
+    const bytes = this.read(6)
+    const cTemp = ((((bytes[0] << 8) + bytes[1]) * 175) / 65535) - 45
+    let temperature
+    switch (s) {
+      case SCALE.CELSIUS:
+        temperature = cTemp
+        break
+      case SCALE.FAHRENHEIT:
+        temperature = cTemp * 1.8 + 32
+        break
+      case SCALE.KELVIN:
+        temperature = cTemp + 273.15
+        break
+      default:
+        throw new Error('Scale not supported')
+    }
+    const humidity = (((bytes[3] << 8) + bytes[4]) * 100) / 65535
+    return {
+      temperature,
+      humidity
+    }
+  }
+}
