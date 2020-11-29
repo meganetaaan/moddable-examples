9a32f2ea7dd749c5a8621b2a05162b7ab40815bb
Shinya Ishikawa <ishikawa@rt-net.jp>
2020-11-30 01:16:18 +0900

    Fix eslintrc space

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,14 @@
 
 module.exports = {
   parser: 'babel-eslint',
-  extends: 'standard',
+  extends: ['standard'],
+  rules: {
+    'space-before-function-paren': ['error', 'never']
+  },
   parserOptions: {
     ecmaVersion: 2019,
     babelOptions: {
       configFile: './babel.config.js'
     }
-  },
+  }
 }
