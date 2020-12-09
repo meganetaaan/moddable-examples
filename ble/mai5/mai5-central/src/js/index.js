07b291238a74052361d910974d9cf7b16cade51b
root <root@sskw-x1.localdomain>
2020-12-09 03:38:49 +0000

    Add images

--- ble/mai5/mai5-central/src/js/index.js
+++ ble/mai5/mai5-central/src/js/index.js
@@ -5,14 +5,18 @@ import BLEClient from './BLEClient.js'
 document.addEventListener('DOMContentLoaded', () => {
   const bleClient = new BLEClient()
   bleClient.onConnected = () => {
-    const status = document.querySelector('.ble-status')
-    status.classList.remove('disconnected')
-    status.classList.add('connected')
+    const contents = document.querySelectorAll('.ble-status,.guide')
+    for (c of contents) {
+      c.classList.remove(['disconnected', 'initial'])
+      c.classList.add('connected')
+    }
   }
   bleClient.onDisconnected = () => {
-    const status = document.querySelector('.ble-status')
-    status.classList.remove('connected')
-    status.classList.add('disconnected')
+    const contents = document.querySelectorAll('.ble-status,.guide')
+    for (c of contents) {
+      c.classList.remove(['connected', 'initial'])
+      c.classList.add('disconnected')
+    }
   }
 
   const connectButton = document.querySelector('.ble-connect-button')
